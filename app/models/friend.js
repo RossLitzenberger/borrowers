0847d891b77f0c891dfba80254f9d20bb4050b52
Ross Litz <codingclasses.049@gmail.com>
2015-11-30 22:21:42 -0600

    Cleaned up some files and link show back to index

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
@@ -6,10 +7,7 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   twitter: DS.attr('string'),
   totalArticles: DS.attr('number'),
-  fullName: Ember.computed('firstName', 'lastName',{
-    get() {
-      return this.get('firstName') + ' ' + this.get('lastName');
-    }
+  fullName: Ember.computed('firstName', 'lastName', function(){
+    return this.get('firstName') + ' ' + this.get('lastName');
   })
-
 });
