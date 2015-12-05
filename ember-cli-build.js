275f5f150850c74056600fa1e035e62a0e74e089
Ross <codingclasses.049@gmail.com>
2015-12-04 21:13:39 -0600

    Setup Dependencies and create a simple partial for nav bar

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,27 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  // Fonts
+  app.import('vendor/fontello/fontello.css');
+  app.import('vendor/fontello/font/fontello.ttf', {
+    destDir: 'font'
+  });
+
+  app.import('vendor/fontello/font/fontello.eot', {
+    destDir: 'font'
+  });
+
+  app.import('vendor/fontello/font/fontello.svg', {
+    destDir: 'font'
+  });
+
+  app.import('vendor/fontello/font/fontello.woff', {
+    destDir: 'font'
+  });
+  // PicnicCSS
+  app.import('bower_components/picnic/releases/plugins.min.css');
+  app.import('bower_components/picnic/releases/picnic.min.css');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
